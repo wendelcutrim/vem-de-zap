0cbd0aa6a69346439b3c409bcb81f405be563ceb
Wendel Cutrim <wendelcutrim@hotmail.com>
2022-06-04 20:02:05 -0300

    Finished the design responsive of the project. Created 3 breakpoints. On mobile devices breakpoints, the close button will stay fixed on the cards and in large devices like notebook, desktops, the close button will appear when the mouse is over the card. Created the event of touch too.

--- public/js/main.js
+++ public/js/main.js
@@ -20,38 +20,40 @@ function sendMessageOnWpp (value){
     const message = value;
     const number = prompt("Qual o número que você deseja enviar esta mensagem?");
     const link = `https://api.whatsapp.com/send?phone=55${number}&text=${message}`;
-
+    if(number.length < 9){
+        window.preventDefault();
+    }
     window.open(link)
 
 }
 
 function createElement (value) {
-    //Criando os elementos
     const li = document.createElement("li");
     const button = document.createElement("button");
     const p = document.createElement("p");
     const i = document.createElement("i");
 
-    //Inserindo os atributos que cada elemento deve conter
     li.setAttribute("id", "card")
     button.setAttribute("class", "close");
     button.innerText = "X";
     p.textContent = value;
     i.setAttribute("class", "fa-brands fa-whatsapp-square");
 
-    //Criando as funções de cada evento
     li.addEventListener("mouseover", () => button.style.display = "block");
     li.addEventListener("mouseout", () => button.style.display = "none");
-    button.addEventListener("click", () => deleteCard(li));
 
-    //Inserindo todos os elementos já com os seus atributos dentro da li
+    li.addEventListener("touchstart", () => button.style.display = "block")
+
+
+    button.addEventListener("click", () => deleteCard(li));
+    
+
     li.appendChild(button);
     li.appendChild(p);
     li.appendChild(i);
-
+    
     li.addEventListener("click", () => sendMessageOnWpp(p.innerText));
 
-    //Inserindo a li com os elementos que já estão com os seus atributos dentro da ul.
     ulCards.appendChild(li);
 }
 
@@ -62,25 +64,3 @@ buttonSubmit.addEventListener("click", (event) => {
     message.value = ""
 });
 
-//Se comentar a função de chamada do card, ele funciona o do botão.
-
-
-/* for(let i = 0; i < liCard.length; i++ ) {
-    liCard[i].onclick = () => {
-        const text = liCard[i].innerText
-        sendMessageOnWpp(text)
-    }
-
-    liCard[i].onmouseover = () => {
-        liCard[i].children[0].style.display = 'block'
-    }
-
-    liCard[i].onmouseout = () => {
-        liCard[i].children[0].style.display = 'none'
-    }
-
-    liCard[i].children[0].onclick = () => {
-        deleteCard(liCard[i])
-    }
-}
- */
